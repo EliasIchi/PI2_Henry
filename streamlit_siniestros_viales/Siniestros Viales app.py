ce41ec0f6c1533f861136ec4291a60427029e67f
Elias Emanuel Almada <124707045+EliasIchi@users.noreply.github.com>
2024-02-13 19:27:19 -0300

    Update Siniestros Viales app.py

--- streamlit_siniestros_viales/Siniestros Viales app.py
+++ streamlit_siniestros_viales/Siniestros Viales app.py
@@ -8,14 +8,3 @@ st.write("¡Bienvenido a la App de Análisis de Accidentes de Tráfico! Esta apl
 # Barra lateral
 
 st.image("https://cdn.motor1.com/images/mgl/mMANEv/s1/seguridad-vial-en-argentina-2021.-foto-telam.jpg", caption="Fuente: Telam")
-
-
-import streamlit as st
-
-# Título e introducción
-st.title("Dashboard de Power BI")
-
-# Insertar el dashboard de Power BI utilizando un iframe
-st.components.v1.iframe("https://app.powerbi.com/view?r=eyJrIjoiOWVkYjFhYmUtNjI5Mi00ZmIyLTgyYTItNjcyYzU0Yzg0MWU4IiwidCI6IjgyZDEwOTAwLWM2M2ItNDhhYi1hYTg1LWViMWM2OTJjYThmMSIsImMiOjR9",
-                        width=800,
-                        height=600)
