a8b76f69cab54df394aa5f407543762bf5bba7a6
Elias Emanuel Almada <124707045+EliasIchi@users.noreply.github.com>
2024-02-13 20:38:55 -0300

    Update 05_📽️videos_del_proyecto.py

--- streamlit_siniestros_viales/pages/05_📽️videos_del_proyecto.py
+++ streamlit_siniestros_viales/pages/05_📽️videos_del_proyecto.py
@@ -2,6 +2,18 @@ import streamlit as st
 
 st.title("Videos del Proyecto, realmente entretenido!!!")
 
+
+st.subheader("Cargando datos en database de Mysql")
+
+# URL del video de YouTube
+video_url = "https://www.youtube.com/watch?v=4f2qY6Y7bJU"
+
+# Mostrar el video
+st.video(video_url)
+
+
+
+
 st.subheader("Conexión con MySQL desde powerBI")
 
 # URL del video de YouTube
@@ -18,4 +30,5 @@ video_url = "https://www.youtube.com/watch?v=VEcWNIr7TtM"
 # Mostrar el video
 st.video(video_url)
 
-"https://www.youtube.com/watch?v=VEcWNIr7TtM"
+
+
