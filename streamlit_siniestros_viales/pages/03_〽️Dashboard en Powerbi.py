a65ceb25c4404840520cc2da1df8dc81a3631bc5
Elias Emanuel Almada <124707045+EliasIchi@users.noreply.github.com>
2024-02-16 23:20:06 -0300

    Update 03_〽️Dashboard en Powerbi.py

--- streamlit_siniestros_viales/pages/03_〽️Dashboard en Powerbi.py
+++ streamlit_siniestros_viales/pages/03_〽️Dashboard en Powerbi.py
@@ -6,4 +6,6 @@ st.markdown("***")
 
 
 # Insertar el dashboard de Power BI utilizando un iframe
-st.components.v1.iframe("https://app.powerbi.com/reportEmbed?reportId=65cb74ba-89c1-4087-b879-251dca92e239&autoAuth=true&ctid=82d10900-c63b-48ab-aa85-eb1c692ca8f1")
+st.components.v1.iframe("https://app.powerbi.com/view?r=eyJrIjoiOWVkYjFhYmUtNjI5Mi00ZmIyLTgyYTItNjcyYzU0Yzg0MWU4IiwidCI6IjgyZDEwOTAwLWM2M2ItNDhhYi1hYTg1LWViMWM2OTJjYThmMSIsImMiOjR9",
+                        width=800,
+                        height=600)
